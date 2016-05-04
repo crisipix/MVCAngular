6646118cd48a7f35b7abd652c323ff41ac62beb2
crisipix <wongc5@gmail.com>
2016-05-04 00:30:17 -0400

    Added repeats

--- MVCAngular/app/controllers/animeController.js
+++ MVCAngular/app/controllers/animeController.js
@@ -9,12 +9,19 @@
         var vm = this;
         vm.name = 'Anime Information';
         vm.anime = [{ Name: 'Hello' }, { Name: 'Hello' }, { Name: 'Hello' }];
-
+        vm.fields = [];
 
         $http.get('/MVCAngular/Anime/IndexValues')
             .success(function (data) {
-            console.log(data);
             vm.anime = data;
+            if (vm.anime.length > 0) {
+                for (var prop in vm.anime[0]) {
+                    vm.fields.push(prop);
+                }
+            }
+            console.log(vm.anime);
+            console.log(vm.fields);
+
         });
     }
 
